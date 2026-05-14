3176a705a7fcc19607089df67cb683bd83a0672a
Invitea <invitea.design@gmail.com>
2026-05-13 22:07:33 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
 function getDynamicDate() {
-  const tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate() + 1);
-
-  const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  };
-
-  const dateString = tomorrow.toLocaleDateString('es-MX', options);
-  return dateString.charAt(0).toUpperCase() + dateString.slice(1);
+  return 'Lunes 18 de Mayo';
 }
 
 document.addEventListener('DOMContentLoaded', function() {
